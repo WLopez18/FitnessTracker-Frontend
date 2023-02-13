f7c942869b7e7c6468e4bb387a7dbcb318ca9060
Walter <walter18lopez@gmail.com>
2023-02-13 11:08:02 -0500

    Testing push with comment in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-const App = ()=> {
+const App = () => {
   return (
     <div>
       <h1>Front End</h1>
@@ -12,3 +12,6 @@ const App = ()=> {
 const root = createRoot(document.querySelector('#root'));
 
 root.render(<App />);
+
+
+//Testing Git push - Walter
