f3d5a337957ca27a836d4a5d08b3d429324148c9
Walter <walter18lopez@gmail.com>
2023-02-15 10:58:14 -0500

    Switched to profs template and added activities div containing a list of all activities

--- src/index.js
+++ src/index.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 
 const App = () => {
-  const fetchActivities = () => {
-
-  }
+  const [routines, setRoutines] = useState([]);
+  const [activities, setActivities] = useState([]);
+  useEffect(() => {
+    fetch('https://fitnesstrac-kr.herokuapp.com/api/routines')
+      .then(response => response.json())
+      .then(routines => setRoutines(routines));
+  }, []);
+  useEffect(() => {
+    fetch('https://fitnesstrac-kr.herokuapp.com/api/activities')
+      .then(response => response.json())
+      .then(activities => setActivities(activities));
+  }, []);
   return (
     <div>
-      <h1>Front End</h1>
+      <h1>Fitness Tracker</h1>
+      <div>
+        <h2>Routines ({routines.length})</h2>
+        <ul>
+          {
+            routines.map(routine => {
+              return (
+                <li key={routine.id}>
+                  {routine.name} ({routine.activities.length})
+                </li>
+              );
+            })
+          }
+        </ul>
+      </div>
+      <div>
+        <h2>Activities ({activities.length})</h2>
+        <ul>
+          {
+            activities.map(activity => {
+              return (
+                <li key={activity.id}>
+                  {activity.name}
+                </li>
+              );
+            })
+          }
+        </ul>
+      </div>
     </div>
   );
 };
