a7f3ff6fd132cafae134f7b11e86ee592355a398
Shan <norman_17899@yahoo.com>
2023-02-16 16:41:17 -0600

    changed the spacing

--- src/components/RoutineView.js
+++ src/components/RoutineView.js
@@ -3,36 +3,36 @@ import { Link, useParams } from 'react-router-dom';
 
 const RoutineView = (props) => {
 
-    const {routines} = props
-    const id = useParams().routineId;
-    const routine = routines.find(routine => routine.id === parseInt(id))
+	const {routines} = props
+	const id = useParams().routineId;
+	const routine = routines.find(routine => routine.id === parseInt(id))
 
-    return (
-        <div>
-            {routine ? <div>
-                <h1><Link to={'/routines'}>{ routine.name }</Link></h1>
-                    <p><span>Goals: </span>{routine.goals}</p>
-                    <p><span>Activities: </span></p>       
-                         <ul>
-                            {
-                                routine.activities.map(activity => {
-                                    return (
-                                        <li key={activity.id}>
-                                            <Link to={`/activities/${activity.id}`} >{activity.name}</Link>
-                                            <ul>
-                                                <li>description:{activity.description}</li>
-                                                <li>duration:{activity.duration}</li>
-                                                <li>count:{activity.count}</li>
-                                            </ul>
-                                        </li>
-                                    )
-                                })
-                            }
-                        </ul>
-                    <p><span>Creator By: </span>{routine.creatorName}</p>
-            </div> : <div>null</div>}
-        </div>
-    )
+	return (
+		<div>
+			{routine ? <div>
+				<h1><Link to={'/routines'}>{ routine.name }</Link></h1>
+					<p><span>Goals: </span>{routine.goals}</p>
+					<p><span>Activities: </span></p>       
+							<ul>
+								{
+									routine.activities.map(activity => {
+										return (
+											<li key={activity.id}>
+												<Link to={`/activities/${activity.id}`} >{activity.name}</Link>
+												<ul>
+													<li>description:{activity.description}</li>
+													<li>duration:{activity.duration}</li>
+													<li>count:{activity.count}</li>
+												</ul>
+											</li>
+										)
+									})
+								}
+							</ul>
+					<p><span>Creator By: </span>{routine.creatorName}</p>
+			</div> : <div>null</div>}
+		</div>
+	)
 }
 
 export default RoutineView;
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 
 const Login = (props) => {
-    const {user, setUser, tokenLogin} = props    
+const {user, setUser, tokenLogin} = props    
 const [username, setUsername]  = useState ("")  
 const [password, setPassword]  = useState ("")
 
